be229a5fff2c7fafb0de9b7ca025321e2e2b7d53
Douglas Willian Dias <douglas.wdias@hotmail.com>
2024-10-24 18:23:48 -0300

    fix, user registration: password requirements validation

--- bugs/Dima.Core/Requests/Account/RegisterRequest.cs
+++ bugs/Dima.Core/Requests/Account/RegisterRequest.cs
@@ -4,10 +4,12 @@ namespace Dima.Core.Requests.Account;
 
 public class RegisterRequest : Request
 {
-    [Required(ErrorMessage = "E-mail")]
-    [EmailAddress(ErrorMessage = "E-mail inválido")]
+    [Required(ErrorMessage = "E-mail não pode ser vazio.")]
+    [EmailAddress(ErrorMessage = "E-mail inválido.")]
     public string Email { get; set; } = string.Empty;
 
-    [Required(ErrorMessage = "Senha Inválida")]
+    [Required(ErrorMessage = "Senha não pode ser vazia.")]
+    [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,32}$", 
+        ErrorMessage = @"Senha precisa conter entre 8 e 32 caracteres com números, letras e ao menos 1 caracter especial.")]
     public string Password { get; set; } = string.Empty;
 }
