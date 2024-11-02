ccfecae858f89b8187cb548c6a45aa55a0ca4ed6
Douglas Willian Dias <douglas.wdias@hotmail.com>
2024-11-02 20:10:21 -0300

    test, stringExtensions: implement stringExtension test

--- unit-tests/Balta.Domain.Test/SharedContext/Extensions/StringExtensionsTests.cs
+++ unit-tests/Balta.Domain.Test/SharedContext/Extensions/StringExtensionsTests.cs
@@ -1,9 +1,18 @@
 using Balta.Domain.SharedContext.Extensions;
+using Bogus;
+using FluentAssertions;
 
 namespace Balta.Domain.Test.SharedContext.Extensions;
 
 public class StringExtensionsTests
 {
+    private readonly Faker _faker = new Faker();
     [Fact]
-    public void ShouldGenerateBase64FromString() => Assert.Fail();
+    public void ShouldGenerateBase64FromString()
+    {
+        var stringToTest = _faker.Lorem.Sentence();
+        var result = stringToTest.ToBase64();
+
+        result.Should().NotBeNull();
+    }
 }
